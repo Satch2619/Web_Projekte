d6a3a1c654ec013f470e9c6b77237b8aa4d0c758
Satch2619 <ninho13@hotmail.de>
2022-08-29 22:08:05 +0200

    fehler dialog.js

--- Personal Trainer/Backend/server.js
+++ Personal Trainer/Backend/server.js
@@ -4,6 +4,12 @@ const cors = require("cors");
 const app =  express();
 const port = 3000;
 
+const corsOptions = {
+  origin: "http://localhost:5500",
+};
+
+app.use(cors(corsOptions));
+
 app.post("/", (request, response) => { 
   response.json("Vielen Dank!");
 })
