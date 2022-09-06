0230def93b392497c1fdacba943c764d9575ecbb
Satch2619 <ninho13@hotmail.de>
2022-09-06 21:45:19 +0200

    555626

--- Personal Trainer/Backend/server.js
+++ Personal Trainer/Backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const multer =require("multer");
+
 
 const app =  express();
 const port = 3000;
@@ -9,9 +9,7 @@ const corsOptions = {
   origin: "http://localhost:5500",
 };
 
-app.use(cors("*"));
-
-const upload = multer();
+app.use(cors(corsOptions));
 
 app.post("/", upload.none(), (request, response) => { 
   console.log(request.body);
